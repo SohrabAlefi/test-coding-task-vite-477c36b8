f2323fa6545c811858d31e4a3ebdf4cbc187b9cd
Sohrab <sohrab@nexaai.dev>
2026-03-17 23:46:12 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App(){
+  return (
+    <div style={{fontFamily: 'system-ui, sans-serif', padding: 40}}>
+      <h1>Hello from Test Vite React App</h1>
+      <p>This is an automated build for the MC workflow test.</p>
+    </div>
+  )
+}
